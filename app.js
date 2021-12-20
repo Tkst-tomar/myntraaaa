fee26732cdc857108af538142a2c385970d28d7f
Tkst-tomar <92792070+Tkst-tomar@users.noreply.github.com>
2021-12-20 21:10:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var abc = []
+var data = [{name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"},
+            {name:"Shirt 1", price:1200, gender:"male", size:"L", path:"Images/img (1).jpg"}]
+function getvalue(value){
+    abc[abc.length]=value
+    console.log(abc)
+}
+
+
+
+document.getElementById("btn").onclick = function(){
+    let string = document.getElementById("name").value
+
+}
